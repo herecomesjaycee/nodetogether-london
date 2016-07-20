c128f3092471bda23b2703ad4f7aee8714d6b071
herecomesjaycee <jaycee.cheong04@gmail.com>
2016-07-20 21:31:28 +0100

    install motivations, pick-one and now /hello display motivational one-liner

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 var Express = require('express');
+const motivations = require('motivations')
+const pickOne = require('pick-one');
 var app = Express();
+
 app.get('/hello', function(request,response){
 	console.log("request to /hello");
-	response.send("Hello!")
+	response.send(pickOne(motivations));
 });
 
 module.exports = app;
